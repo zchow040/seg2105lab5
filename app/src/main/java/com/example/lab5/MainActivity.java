7bc7a60b9520cc06ea52f73e28b6f74923792e80
Zain Chowdhry <zchow040@uottawa.ca>
2024-10-28 20:57:45 -0400

    first commit

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -15,9 +15,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
     List<Product> products;
 
+    DatabaseReference databaseProducts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -64,6 +73,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        databaseProducts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                products.clear();
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Product product = postSnapshot.getValue(Product.class);
+                    products.add(product);
+                }
+                ProductList productsAdapter = new ProductList(MainActivity.this,products);
+                listViewProducts.setAdapter(productsAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
@@ -106,16 +132,35 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateProduct(String id, String name, double price) {
 
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+        Product product = new Product(id,name,price);
+        dR.setValue(product);
+        Toast.makeText(getApplicationContext(),"Product Updated",Toast.LENGTH_LONG).show();
+
     }
 
     private void deleteProduct(String id) {
 
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+        dR.removeValue();
+        Toast.makeText(getApplicationContext(),"Product Deleted",Toast.LENGTH_LONG).show();
+
     }
 
     private void addProduct() {
 
-        Toast.makeText(this, "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+        String name = editTextName.getText().toString().trim();
+        double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseProducts.push().getKey();
+            Product product = new Product(id,name,price);
+            databaseProducts.child(id).setValue(product);
+            editTextName.setText("");
+            editTextPrice.setText("");
+            Toast.makeText(this,"Product added",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this,"Please enter a name",Toast.LENGTH_LONG).show();
+        }
     }
 }
